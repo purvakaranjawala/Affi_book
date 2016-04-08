000599e9c8def1b71dda0b2aca7177b576fb2591
rordev1 <purvakaranjawala>
2016-04-08 12:37:06 +0530

    fourth commit

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -35,21 +35,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # version :thumbnail do
   #   process :resize_to_fill => [50, 50]
   # end
-
-  #  version :large_avatar do
-  #   # returns a 150x150 image
-  #   process :resize_to_fill => [150, 150]
-  # end
-  # version :medium_avatar do
-  #   # returns a 50x50 image
-  #   process :resize_to_fill => [50, 50]
-  # end
-  
-  # version :small_avatar do
-  #   # returns a 35x35 image
-  #   process :resize_to_fill => [35, 35]
-  # end
-
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
    def extension_white_list
